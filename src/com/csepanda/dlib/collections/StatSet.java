2b3fed4fdb80c8f389c082dd6c6deb8d4a62c975
Andrey Bova <andrewbova@yandex.ru>
2016-12-13 10:07:16 +0300

    Update StatSet implementation and api for previous dlib changes

--- src/com/csepanda/dlib/collections/StatSet.java
+++ src/com/csepanda/dlib/collections/StatSet.java
@@ -12,13 +12,13 @@ import java.util.Iterator;
 import java.util.HashMap;
 import java.util.NoSuchElementException;
 
+import com.csepanda.dlib.data.Node;
 /**
  * Specific realization of interface Set.
  * Head feature is the order of elements:
  * All elements keeps in order by frequency of their adding.
- * Also it's better to keep in this collection immutable object,
- * because realization(HashMap is used) promise that correct
- * contains/adding.
+ * Be careful with mutable objects, in cause of usage hash-algoritms
+ * it might be a problem for correct adding, removing and storing.
  * @param <E> the type of elements maintained by this set
  * @author Andrey Bova
  * 
@@ -26,10 +26,10 @@ import java.util.NoSuchElementException;
 
 public class StatSet<E> extends AbstractSet<E> implements Set<E> {
     
-    private int        size;
-    private Node<E>    head;
-    private Node<E>    tail;
-    private HashMap<E, Node<E>> containsMap;
+    private int           size;
+    private LinkedNode<E> head;
+    private LinkedNode<E> tail;
+    private HashMap<E, LinkedNode<E>> containsMap;
     
     /**
      * Constructs a new, empty set     
@@ -39,7 +39,7 @@ public class StatSet<E> extends AbstractSet<E> implements Set<E> {
     }
     
     /**
-     * Constructs a new set contatins the elements in the specified collection.
+     * Constructs a new set contains the elements in the specified collection.
      *
      * @param c the collection whose elements are to be placed into this set
      * @throws NullPointerException if the specified collection is null
@@ -55,7 +55,7 @@ public class StatSet<E> extends AbstractSet<E> implements Set<E> {
      * @return the most often added element of this set
      */
     public E getTop() {
-        return head.item;
+        return head.getValue();
     }
     
     /**
@@ -126,7 +126,7 @@ public class StatSet<E> extends AbstractSet<E> implements Set<E> {
      * @return <tt>true</tt> if th set contained the specified element
      */ 
     public boolean remove(Object o) {
-        final Node<E> node = containsMap.get(o);
+        final LinkedNode<E> node = containsMap.get(o);
         
         if (node == null) {
             return false;
@@ -146,17 +146,17 @@ public class StatSet<E> extends AbstractSet<E> implements Set<E> {
     }
    
     /**
-     * Return an iterator over the elements and their weights in this set.
+     * Return an iterator over the StatSet's nodes.
      *
-     * @return an iterator over the elements and their weights in this set
+     * @return an iterator over the nodes in this set
      */
-    public StatSetIterator statisticIterator() {
+    public NodeIterator<E> nodeIterator() {
         return new StatSetIterator();
     }
 
     private void addLast(E e) {
-        final Node<E> last    = tail;
-        final Node<E> newNode = new Node<>(last, e, null);
+        final LinkedNode<E> last    = tail;
+        final LinkedNode<E> newNode = new LinkedNode<>(last, e, null);
         tail = newNode;
         if (last == null) {
             head = newNode;
@@ -169,21 +169,21 @@ public class StatSet<E> extends AbstractSet<E> implements Set<E> {
 
     private void raise(E e) {
         int index, weight;
-        Node<E> current = containsMap.get(e);
-        Node<E> parent  = current.prev;
+        LinkedNode<E> current = containsMap.get(e);
+        LinkedNode<E> parent  = current.prev;
                 
-        current.weight++;
+        current.incrementWeight();
         if (parent        == null || 
-            parent.weight >= current.weight) {
+            current.isLighterThan(parent)) {
             return;
         } // skip if doesn't need raise
         
-        if (head.weight <= current.weight) {
+        if (!current.isLighterThan(head)) {
             parent = head;
         }
 
         while (parent != null &&
-               parent.weight < current.weight) {
+               parent.isLighterThan(current)) {
             parent = parent.prev;
         } // searching for right-weight place
              
@@ -191,14 +191,14 @@ public class StatSet<E> extends AbstractSet<E> implements Set<E> {
         linkAfter(current, parent);
     }
 
-    private void linkAtHead(Node<E> node) {
+    private void linkAtHead(LinkedNode<E> node) {
         node.next = head;
         node.prev = null;
         head.prev = node;
         head = node;
     }
 
-    private void linkAfter(Node<E> node, Node<E> parent) {
+    private void linkAfter(LinkedNode<E> node, LinkedNode<E> parent) {
         if (parent == null) {
             linkAtHead(node);
         } else {            
@@ -212,9 +212,9 @@ public class StatSet<E> extends AbstractSet<E> implements Set<E> {
         }
     }
 
-    private void unlinkNode(Node<E> node) {
-        final Node<E> prev = node.prev;
-        final Node<E> next = node.next;
+    private void unlinkNode(LinkedNode<E> node) {
+        final LinkedNode<E> prev = node.prev;
+        final LinkedNode<E> next = node.next;
 
         if (prev == null) {
             head = next;
@@ -231,39 +231,39 @@ public class StatSet<E> extends AbstractSet<E> implements Set<E> {
         }        
     }
 
-    private void removeNode(Node<E> node) {
+    private void removeNode(LinkedNode<E> node) {
         unlinkNode(node); 
-        containsMap.remove(node.item);
-        node.item = null; 
+        containsMap.remove(node.getValue());
         size--; 
     }    
 
-    private class Node<E> {
-        E       item;      
-        int     weight;
-        Node<E> next;
-        Node<E> prev;
+    private class LinkedNode<E> extends Node<E> {
+        LinkedNode<E> next;
+        LinkedNode<E> prev;
 
-        Node(Node<E> prev, E element, Node<E> next) {
-            this.weight = 1;
-            this.item   = element;
+        LinkedNode(LinkedNode<E> prev, E element, LinkedNode<E> next) {
+            super(element, 1);
             this.next   = next;
-            this.prev   = prev;
+            this.prev   = prev;            
+        }
+
+        public int incrementWeight() {
+            return weight++;
         }
     }
     
     /*                                                               */
     /*                          iterators                            */
     /*                                                               */
-    private abstract class NodeIterator {
-        protected Node<E> next    = head;
-        protected Node<E> current;
+    private abstract class LinkedNodeIterator {
+        protected LinkedNode<E> next = head;
+        protected LinkedNode<E> current;
 
         public final boolean hasNext() {   
             return current != tail;
         }
     
-        protected final Node<E> nextNode() {
+        protected final LinkedNode<E> nextNode() {
             if (next == null) {
                 throw new NoSuchElementException();
             }
@@ -282,9 +282,9 @@ public class StatSet<E> extends AbstractSet<E> implements Set<E> {
         }
     }
 
-    private class ElementsIterator extends NodeIterator 
+    private class ElementsIterator extends LinkedNodeIterator 
                                    implements Iterator<E> {
-        public E next() { return nextNode().item; }
+        public E next() { return nextNode().getValue(); }
     }
 
 
@@ -292,34 +292,10 @@ public class StatSet<E> extends AbstractSet<E> implements Set<E> {
      * Extended iterator over the elements and their weights in StatSet.
      *
      */     
-    private class StatSetIterator extends ElementsIterator 
-                                  implements Iterator<E> {
-        /**
-         * Returns current element's weight.
-         *
-         * @return current element's weight.
-         * @throws IllegalStateException            {@inheritDoc}
-         */
-        public int getWeight() {
-            if (current == null) {
-                throw new IllegalStateException();
-            }
-
-            return current.weight;
-        }
-
-        /**
-         * Returns current element.
-         *
-         * @return current element.
-         * @throws IllegalStateException            {@inheritDoc}
-         */
-        public E getValue() {
-            if (current == null) {
-                throw new IllegalStateException();
-            }
-
-            return current.item;
+    private class StatSetIterator extends LinkedNodeIterator 
+                                  implements NodeIterator<E> {
+        public Node<E> next() {
+            return nextNode();
         }
     }
     
